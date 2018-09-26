23db521297327e3074a6011dc1ad5876e62a3fb9
Yuuki Kakimoto <kakimoto.yuuki@moneyforward.co.jp>
2018-09-26 18:41:25 +0900

    検証等の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password validations: true
+  
+  validates :name, presence:true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 end
