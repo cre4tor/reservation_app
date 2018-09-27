644b4fefed55b24e643c62c5fa91b623b8db3067
Yuuki Kakimoto <kakimoto.yuuki@moneyforward.co.jp>
2018-09-27 18:03:32 +0900

    UserモデルとFpモデル、Clientモデルを関連付け

--- app/models/fp.rb
+++ app/models/fp.rb
@@ -1,2 +1,3 @@
 class Fp < ApplicationRecord
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  has_one :fp, dependent: :destroy
+  accepts_nested_attributes_for :fp, allow_destroy: true
+  has_one :client, dependent: :destroy
+  accepts_nested_attributes_for :client, allow_destroy: true
+  
   has_secure_password validations: true
   
   validates :name, presence:true, uniqueness: true
