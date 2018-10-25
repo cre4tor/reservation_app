08c663f2fdd49fc651fcf0671bb882f515ec65fb
Yuuki Kakimoto <kakimoto.yuuki@moneyforward.co.jp>
2018-10-25 17:02:28 +0900

    name,email属性に対するvalidationの追加

--- app/models/fp.rb
+++ app/models/fp.rb
@@ -7,6 +7,8 @@ class Fp < ApplicationRecord
 
   has_secure_password validations: true
   
-  validates :name, presence:true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
+  validates :name, presence:true, length: { maximum: 31 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true,
+                    length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
 end
