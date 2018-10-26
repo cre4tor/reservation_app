839ba123157415681f891922d49b7477a7b8a137
Yuuki Kakimoto <kakimoto.yuuki@moneyforward.co.jp>
2018-10-26 16:51:44 +0900

    name,email属性に対するvalidation等の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,13 @@ class User < ApplicationRecord
   accepts_nested_attributes_for :financial_planner, allow_destroy: true
   has_one :client, dependent: :destroy
   accepts_nested_attributes_for :client, allow_destroy: true
-
+  has_secure_password validations: true
+  
+  validates :name, presence:true, length: { maximum: 31 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true,
+                    length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+                        
   def full_name
     "#{first_name} #{last_name}"
   end
