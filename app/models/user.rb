08ebe8e61516d3f2d05d8e085eb50e8e43964160
Yuuki Kakimoto <kakimoto.yuuki@moneyforward.co.jp>
2018-10-26 16:55:37 +0900

    passwordの最小文字数を設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ApplicationRecord
   accepts_nested_attributes_for :client, allow_destroy: true
   has_secure_password validations: true
   
-  validates :name, presence:true, length: { maximum: 31 }
+  validates :password, presence: true, length: {minimum: 6}
+  validates :name, presence: true, length: { maximum: 31 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true,
                     length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
