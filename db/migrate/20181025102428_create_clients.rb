e8e68805e1ce05e4f9fb09b346db4bc7c3e410f7
Yuuki Kakimoto <kakimoto.yuuki@moneyforward.co.jp>
2018-10-26 16:36:52 +0900

    null: falseの追加

--- db/migrate/20181025102428_create_clients.rb
+++ db/migrate/20181025102428_create_clients.rb
@@ -1,7 +1,7 @@
 class CreateClients < ActiveRecord::Migration[5.1]
   def change
     create_table :clients do |t|
-      t.integer :user_id
+      t.integer :user_id, null: false
 
       t.timestamps
     end
