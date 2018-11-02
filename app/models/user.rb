259d46777a818046019d0b484e8babf3e21ab07c
Yuuki Kakimoto <kakimoto.yuuki@moneyforward.co.jp>
2018-11-02 18:10:48 +0900

    digestが存在しない場合のエラーに対応

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,7 @@ class User < ApplicationRecord
   end
 
   def authentiated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
