67efbf113be1d350e847e2e348890e8bf35c7dbb
Yuuki Kakimoto <kakimoto.yuuki@moneyforward.co.jp>
2018-11-15 19:25:38 +0900

    FP,Clientを区別して生成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    if params[:type] == 'financial_planner'
+      @user.build_financial_planner(@user_id)
+    elsif params[:type] == 'client'
+      @user.build_client(@user_id)
+    end
     if @user.save
       log_in @user
       flash[:success] = 'Welcome!'
@@ -24,4 +29,5 @@ class UsersController < ApplicationController
     params.require(:user).permit(:first_name, :last_name, :email,
                                  :password, :password_confirmation)
   end
+
 end
