18bdbdbca0a81257d2f43db8ab1c381ebbd600f6
Yuuki Kakimoto <kakimoto.yuuki@moneyforward.co.jp>
2018-11-20 15:49:50 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181120032811) do
+ActiveRecord::Schema.define(version: 20181120064347) do
 
   create_table "clients", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "user_id", null: false
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20181120032811) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "financial_planner_id"
+    t.date "reservation_date"
     t.index ["financial_planner_id"], name: "index_time_frames_on_financial_planner_id"
   end
 
