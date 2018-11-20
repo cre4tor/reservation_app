7ddd12c5ae0eeb8436ae7c4d605a38af1dcb141e
Yuuki Kakimoto <kakimoto.yuuki@moneyforward.co.jp>
2018-11-20 12:33:20 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181120031940) do
+ActiveRecord::Schema.define(version: 20181120032811) do
 
   create_table "clients", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "user_id", null: false
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 20181120031940) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_financial_planners_on_user_id", unique: true
+  end
+
+  create_table "reservations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "client_id"
+    t.integer "time_frame_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["client_id"], name: "index_reservations_on_client_id"
+    t.index ["time_frame_id"], name: "index_reservations_on_time_frame_id"
   end
 
   create_table "time_frames", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
