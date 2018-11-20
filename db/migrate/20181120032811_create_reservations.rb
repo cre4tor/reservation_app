d1e4f8932547a32e1d09e36f34381dcb5bdabec2
Yuuki Kakimoto <kakimoto.yuuki@moneyforward.co.jp>
2018-11-20 12:29:03 +0900

    Reservationモデルの生成 rails g model Reservation client_id:integer:index time_frame_id:integer:index

---
+++ db/migrate/20181120032811_create_reservations.rb
@@ -0,0 +1,12 @@
+class CreateReservations < ActiveRecord::Migration[5.1]
+  def change
+    create_table :reservations do |t|
+      t.integer :client_id
+      t.integer :time_frame_id
+
+      t.timestamps
+    end
+    add_index :reservations, :client_id
+    add_index :reservations, :time_frame_id
+  end
+end
