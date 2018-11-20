c25975b2715f84af8f70048a01c21de6b9cae66d
Yuuki Kakimoto <kakimoto.yuuki@moneyforward.co.jp>
2018-11-20 14:40:12 +0900

    選択できる時刻範囲を設定

--- app/controllers/time_frames_controller.rb
+++ app/controllers/time_frames_controller.rb
@@ -1,4 +1,12 @@
 class TimeFramesController < ApplicationController
   def new
+    #配列を生成
+    @weekday_start_time_array = []
+    @saturday_start_time_array = []
+
+    #平日の開始時刻範囲を設定[10:00~17:30]
+    @weekday_start_time_array = 16.times.map.each_with_index {|i| Time.parse("10:00") + 30.minutes * i}
+    #土曜日の開始時刻範囲を設定[11:00~14:30]
+    @saturday_start_time_array = 8.times.map.each_with_index {|i| Time.parse("11:00") + 30.minutes * i}
   end
 end
