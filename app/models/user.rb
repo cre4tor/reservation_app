2d5e869a42ca13d0d1694539352e2553d6ddbc17
Yuuki Kakimoto <kakimoto.yuuki@moneyforward.co.jp>
2018-11-21 16:46:53 +0900

    update_attributesの記述を間違っていたので修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,7 +31,7 @@ class User < ApplicationRecord
 
   def remember
     self.remember_token = User.new_token
-    update_attributes(:remember_digest, User.digest(remember_token))
+    update_attributes(remember_digest: User.digest(remember_token))
   end
 
   def authentiated?(remember_token)
