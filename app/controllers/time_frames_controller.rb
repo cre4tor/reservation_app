08f415b5f2191f3b1017dca311e48a1327a96807
Yuuki Kakimoto <kakimoto.yuuki@moneyforward.co.jp>
2018-11-22 17:19:02 +0900

    change to array of time only

--- app/controllers/time_frames_controller.rb
+++ app/controllers/time_frames_controller.rb
@@ -1,5 +1,5 @@
 class TimeFramesController < ApplicationController
-  before_action :logged_in_financial_planner, only[:create]
+  before_action :logged_in_financial_planner, only: [:new, :create]
 
   def new
     @time_frame = TimeFrame.new
@@ -9,19 +9,30 @@ class TimeFramesController < ApplicationController
     @saturday_start_time_array = []
 
     #平日の開始時刻範囲を設定[10:00~17:30]
-    @weekday_start_time_array = 16.times.map.each_with_index {|i| Time.parse("10:00") + 30.minutes * i}
+    @weekday_start_time_array = 16.times.map.each_with_index {|i| Tod::TimeOfDay.parse("10:00") + 30.minutes * i}
     #土曜日の開始時刻範囲を設定[11:00~14:30]
-    @saturday_start_time_array = 8.times.map.each_with_index {|i| Time.parse("11:00") + 30.minutes * i}
+    @saturday_start_time_array = 8.times.map.each_with_index {|i| Tod::TimeOfDay.parse("11:00") + 30.minutes * i}
   end
 
   def create
     @time_frame = @financial_planner.time_frames.build(time_frame_params)
     if @time_frame.save
-      flash[:success] = "Reservation success!"
+      flash.now[:success] = "Reservation success!"
       redirect_to @current_user
     else
-      flash[:danger] = "Reservation failed..."
+      flash.now[:danger] = "Reservation failed..."
       render 'new'
+    end
+  end
+
+  def logged_in_financial_planner
+    unless logged_in?
+      flash[:danger] = "Please log in"
+      redirect_to login_url
+    end
+    unless current_user.financial_planner?
+      flash[:danger] = "Permission denied"
+      redirect_to @current_user
     end
   end
 
@@ -31,14 +42,4 @@ class TimeFramesController < ApplicationController
       params.require(:time_frame).permit(:reservation_date, :start_time, :end_time)
     end
 
-    def logged_in_financial_plannner
-      unless logged_in?
-        flash[:danger] = "Please log in"
-        redirect_to login_url
-      end
-      unless financial_plannner?
-        flash[:danger] = "Permission denied"
-        redirect_to @current_user
-      end
-    end
-end
+  end
