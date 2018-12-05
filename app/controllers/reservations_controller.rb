15e345f092b961e1410ecb654d8adde834d72aca
Yuuki Kakimoto <kakimoto.yuuki@moneyforward.co.jp>
2018-12-05 18:14:06 +0900

    add reservation title setting field

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,4 +2,23 @@ class ReservationsController < ApplicationController
   def new
     @reservation = Reservation.new
   end
+
+  def create
+    @time_frame = TimeFrame.find_by(id: current_user)
+    binding.pry
+    @reservation = @time_frame.build_reservation(reservation_params)
+    if @reservation.save
+      flash[:success] = "Reservation SUCCESS!"
+      redirect_to current_user
+    else
+      flash[:danger] = "Reservation failed..."
+      redirect_to new_reservation_path
+    end
+  end
+
+  private
+
+    def reservation_params
+      params.require(:reservation).permit(:title)
+    end
 end
