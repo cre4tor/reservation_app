bf7e62181568fd53a6961c88746e04ddaefaa74b
Yuuki Kakimoto <kakimoto.yuuki@moneyforward.co.jp>
2018-12-06 18:44:41 +0900

    finish reservation function on client side

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,13 +2,18 @@ class ReservationsController < ApplicationController
   before_action :logged_in_client, only: [:new, :create]
 
   def new
-    @time_frames = TimeFrame.order(:start_time, :reservation_date)
+    @time_frames = TimeFrame.all.order(:start_time, :reservation_date)
     @reservation = Reservation.new
   end
 
   def create
+    @client = Client.find_by(user_id: @current_user)
     @time_frame = TimeFrame.find_by(id: reservation_params)
-    @reservation = @time_frame.build_reservation
+    @reservation = @time_frame.build_reservation(client_id: @client)
+
+    binding.pry
+    @time_frame.reservation.client_id = @client.id
+
     binding.pry
     if @reservation.save
       flash[:success] = "Reservation SUCCESS!"
@@ -33,7 +38,7 @@ class ReservationsController < ApplicationController
   private
 
     def reservation_params
-      param = params.require(:reservation).permit(:time_frame_id)
-      param.match(/[0-9].*/)
+      param = params.require(:reservation).permit(:time_frame_id).to_s
+      param.delete("^0-9").to_i
     end
 end
