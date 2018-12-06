c7f9defb4c75962aa0f822111416f7e790a096d8
Yuuki Kakimoto <kakimoto.yuuki@moneyforward.co.jp>
2018-12-06 17:10:26 +0900

    create reservation function

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,11 +1,14 @@
 class ReservationsController < ApplicationController
+  before_action :logged_in_client, only: [:new, :create]
+
   def new
+    @time_frames = TimeFrame.order(:start_time, :reservation_date)
     @reservation = Reservation.new
   end
 
   def create
-    @time_frame = TimeFrame.findby(id: current_user)
-    @reservation = @time_frame.build_reservation(reservation_params)
+    @time_frame = TimeFrame.find_by(id: reservation_params)
+    @reservation = @time_frame.build_reservation
     binding.pry
     if @reservation.save
       flash[:success] = "Reservation SUCCESS!"
@@ -16,9 +19,21 @@ class ReservationsController < ApplicationController
     end
   end
 
+  def logged_in_client
+    unless logged_in?
+      flash[:danger] = "Please log in"
+      redirect_to login_url
+    end
+    unless current_user.client?
+      flash[:danger] = "Permission denied"
+      redirect_to @current_user
+    end
+  end
+
   private
 
     def reservation_params
-      params.require(:reservation).permit(:title)
+      param = params.require(:reservation).permit(:time_frame_id)
+      param.match(/[0-9].*/)
     end
 end
