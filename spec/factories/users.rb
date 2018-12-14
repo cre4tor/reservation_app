89acff318b341edd3b8228f864b5d34b85561dad
Yuuki Kakimoto <kakimoto.yuuki@moneyforward.co.jp>
2018-12-14 14:58:49 +0900

    add test user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,9 @@
 
 FactoryBot.define do
   factory :user do
+    sequence(:first_name) { |n| "john#{n}" }
+    sequence(:last_name) { |n| "john#{n}" }
+    sequence(:email) { |n| "john#{n}@john.com" }
+    sequence(:password) { |n| "johnjohn#{n}" }
   end
 end
