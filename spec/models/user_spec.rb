2c0270b000d97403c846d83b03fd295e17746247
Yuuki Kakimoto <kakimoto.yuuki@moneyforward.co.jp>
2018-12-14 14:03:38 +0900

    create user model presence test & unique email test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,14 +3,56 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # 姓、名、メール、パスワードがあれば有効な状態であること
-  it 'is valid with a first name, last name, email, and password'
-  # 名がなければ無効な状態であること
-  it 'is invalid without a first name'
-  # 姓がなければ無効な状態であること
-  it 'is invalid without a last name'
-  # メールアドレスがなければ無効な状態であること
-  it 'is invalid without an email address'
+  # 存在性の検証
+  describe 'presence' do
+    # 姓、名、メール、パスワードがあれば有効な状態であること
+    it 'is valid with a first name, last name, email, and password' do
+      user = User.new(
+        first_name: 'john',
+        last_name:  'john',
+        email:      'john@john.com',
+        password:   'johnjohnjohn'
+      )
+      expect(user).to be_valid
+    end
+
+    # 名がなければ無効な状態であること
+    it 'is invalid without a first name' do
+      user = User.new(first_name: nil)
+      user.valid?
+      expect(user.errors[:first_name]).to include("can't be blank")
+    end
+
+    # 姓がなければ無効な状態であること
+    it 'is invalid without a last name' do
+      user = User.new(last_name: nil)
+      user.valid?
+      expect(user.errors[:last_name]).to include("can't be blank")
+    end
+
+    # メールアドレスがなければ無効な状態であること
+    it 'is invalid without an email address' do
+      user = User.new(email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+  end
+
   # 重複したメールアドレスなら無効な状態であること
-  it 'is invalid with a duplicate email address'
+  it 'is invalid with a duplicate email address' do
+    User.create(
+      first_name: 'john',
+      last_name:  'john',
+      email:      'john@john.com',
+      password:   'johnjohnjohn'
+    )
+    user = User.new(
+      first_name: 'john',
+      last_name:  'john',
+      email:      'john@john.com',
+      password:   'johnjohnjohn'
+    )
+    user.valid?
+    expect(user.errors[:email]).to include('has already been taken')
+  end
 end
