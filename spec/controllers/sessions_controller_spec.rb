c78ee0e4962bc2254df2877566d0d776852531be
Yuuki Kakimoto <kakimoto.yuuki@moneyforward.co.jp>
2018-12-25 16:53:39 +0900

    add test summary

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,4 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  describe 'POST #create' do
+    context 'correct user' do
+      it 'redirected to user page' do
+      end
+    end
+    context 'incorrect user' do
+      it 'redirected to :new page' do
+      end
+    end
+  end
+  describe 'DELETE #destroy' do
+  end
 end
