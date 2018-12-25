eff39189c6fb894abe5e7a4acb5fec9a18744c3e
Yuuki Kakimoto <kakimoto.yuuki@moneyforward.co.jp>
2018-12-25 15:54:51 +0900

    add test summary

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,4 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  describe 'GET #show' do
+  end
+  describe 'GET #new' do
+  end
+  describe 'POST #create' do
+  end
 end
