35accb6a19b1fbceef7e273255c5cc4991c0a708
Yuuki Kakimoto <kakimoto.yuuki@moneyforward.co.jp>
2018-12-25 16:00:34 +0900

    add show test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,6 +4,20 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
   describe 'GET #show' do
+    let(:user) { create(:user) }
+    before { get :show, params: { id: user.id } }
+
+    it 'has a 200 status code' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'assingns @user' do
+      expect(assigns(:user)).to eq user
+    end
+
+    it 'renders the :show template' do
+      expect(response).to render_template :show
+    end
   end
   describe 'GET #new' do
   end
