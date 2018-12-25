1cce1204531563abf7f35c35bb1d9da1bacacbe4
Yuuki Kakimoto <kakimoto.yuuki@moneyforward.co.jp>
2018-12-25 18:15:56 +0900

    add correct user test

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -4,8 +4,12 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
   describe 'POST #create' do
+    let(:user_attributes) { attributes_for(:user) }
     context 'correct user' do
       it 'redirected to user page' do
+        post :create, params: { user: user_attributes }
+        user = User.last
+        expect(response).to redirect_to user_path(user)
       end
     end
     context 'incorrect user' do
