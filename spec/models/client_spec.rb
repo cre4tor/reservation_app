bb74616443074921609fc8ad650c0e25633a53a0
Yuuki Kakimoto <kakimoto.yuuki@moneyforward.co.jp>
2019-01-21 18:49:40 +0900

    add client_spec & financial_planner_spec

---
+++ spec/models/client_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  # Client 依存関係
+  describe 'client associations' do
+    # association type
+    describe 'belongs_to' do
+    end
+    describe 'has_many' do
+    end
+  end
+end
---
+++ spec/models/financial_planner_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  # FinancialPlanner 依存関係
+  describe 'financial_planner associations' do
+    # association type
+    describe 'belongs_to' do
+    end
+    describe 'has_many' do
+    end
+  end
+end
