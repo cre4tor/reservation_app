e11e9124100af5c3f14456582ce91c3a17858f41
Yuuki Kakimoto <kakimoto.yuuki@moneyforward.co.jp>
2019-01-22 14:51:37 +0900

    correction of association test

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -2,13 +2,15 @@
 
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
+RSpec.describe Client, type: :model do
   # Client 依存関係
   describe 'client associations' do
     # association type
     describe 'belongs_to' do
+      it { should belong_to(:user) }
     end
     describe 'has_many' do
+      it { should have_many(:reservations).dependent(:destroy) }
     end
   end
 end
--- spec/models/financial_planner_spec.rb
+++ spec/models/financial_planner_spec.rb
@@ -2,13 +2,15 @@
 
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
+RSpec.describe FinancialPlanner, type: :model do
   # FinancialPlanner 依存関係
   describe 'financial_planner associations' do
     # association type
     describe 'belongs_to' do
+      it { should belong_to(:user) }
     end
     describe 'has_many' do
+      it { should have_many(:time_frames).dependent(:destroy) }
     end
   end
 end
