9271bf06735b6e8f21f5a14f19d5973479d540e3
Yuuki Kakimoto <kakimoto.yuuki@moneyforward.co.jp>
2019-01-31 17:41:07 +0900

    factory add association models

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,27 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+  factory :time_frame do
+    sequence(:start_time) { |n| Tod::TimeOfDay.parse('10:00') + 30.minutes * n % 10 }
+    reservation_date '2019-05-01'
+  end
+
   factory :user do
     sequence(:first_name) { |n| "john#{n}" }
     sequence(:last_name) { |n| "john#{n}" }
     sequence(:email) { |n| "john#{n}@john.com" }
     sequence(:password) { |n| "johnjohn#{n}" }
+
+    trait :with_fp do
+      after(:build) do |user|
+        user.build_financial_planner
+      end
+    end
+
+    trait :with_client do
+      after(:build) do |user|
+        user.build_client
+      end
+    end
   end
 end
