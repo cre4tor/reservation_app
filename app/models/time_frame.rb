7fe816aa7fc6d411be3baa54597151f7d98ba6e0
cre4tor <41988300+cre4tor@users.noreply.github.com>
2019-02-27 14:41:46 +0900

    Revert "Add validation"

--- app/models/time_frame.rb
+++ app/models/time_frame.rb
@@ -5,7 +5,6 @@ class TimeFrame < ApplicationRecord
   has_one :reservation, dependent: :destroy
   accepts_nested_attributes_for :reservation, allow_destroy: true
 
-  validates :financial_planner_id, uniqueness: { scope: [:reservation_date, :start_time] }
   validates :start_time, presence: true
   validates :end_time, presence: true
 end
